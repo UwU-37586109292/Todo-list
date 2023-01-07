f79ed63b7f16d56b15cd1699840d22fd78d9ff3a
UwU-37586109292 <kikucisko@gmail.com>
2023-01-07 18:07:14 +0100

    Add new todo to project using input form and refresh the list to show current todo list

--- src/GUI/main-canvas.js
+++ src/GUI/main-canvas.js
@@ -1,31 +1,66 @@
 import { projectList } from "../Model/project"
+import { addTodoToCurrentProject } from "../controller/app"
 
 export default function showMainCanvas(){
     const mainContent = document.getElementById('content')
 
     const canvas = document.createElement('main')
+    canvas.setAttribute('id', 'main')
     canvas.classList.add('flex', 'column')
-
-    // pull existing todos
-    appendExistingTodos(canvas)
-
     // Add new todos
     const button = document.createElement('button')
     button.innerText = "Add a task to be done"
     button.addEventListener("click", showAddTaskSection)
     canvas.appendChild(button)
-    
+ 
+    // pull existing todos
+    appendExistingTodos(canvas)
+
     mainContent.appendChild(canvas)
 }
 
 function showAddTaskSection(){
-    alert('this is the add task modal')
+    const canvas = document.getElementById('main')
+    const form = document.createElement('form')
+    form.setAttribute('name', 'addTodo')
+    const inputTodoTitle = document.createElement('input')
+    inputTodoTitle.setAttribute('type', 'text')
+    inputTodoTitle.setAttribute('id','todoTitle')
+    inputTodoTitle.setAttribute('name', 'todoTitle')
+
+    const saveButton = document.createElement('button')
+    saveButton.setAttribute('type', 'submit')
+    saveButton.innerText = 'Save task'
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+        addTodoToCurrentProject(document.getElementById('todoTitle').value)
+    })
+
+    const discardButton = document.createElement('button')
+    discardButton.setAttribute('type', 'button')
+    discardButton.innerText = 'Discard'
+
+    form.appendChild(inputTodoTitle)
+    form.appendChild(saveButton)
+    form.appendChild(discardButton)
+    canvas.appendChild(form)
+}
+
+export function hideAddTaskSection(){
+    const form = document.getElementsByTagName('form')[0]
+    form.remove()
+
+}
+
+export function refreshTodosList(){
+    const canvas = document.getElementById('main')
+    document.querySelector('#main ul').remove()
+    appendExistingTodos(canvas)
 }
 
 function appendExistingTodos(canvas){
     const todoList = document.createElement('ul')
     projectList.getCurrentProject().getAllTasks().forEach(task => {
-        console.log(task)
         const todo = document.createElement('li')
         todo.innerText = task.getTitle()
         todoList.appendChild(todo)
