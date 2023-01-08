d3580bb33e1d073b5c3baf0a74c8a6ac36db68be
UwU-37586109292 <kikucisko@gmail.com>
2023-01-08 09:37:43 +0100

    Add footer content

--- src/GUI/footer.js
+++ src/GUI/footer.js
@@ -1,8 +1,43 @@
-export default function showFooter(){
-    const mainContent = document.getElementById('content')
+import Icon from '../assets/icon.png';
+import IconRevers from '../assets/icon_light.png'
 
+export default function showFooter() {
     const footer = document.createElement('footer')
-    footer.classList.add('flex', 'column')
-    footer.textContent = 'Footer'
+    footer.textContent = 'Created by Zuzka'
+    footer.classList.add('footer', 'flex', 'center')
+
+    const githubLink = document.createElement('a')
+    githubLink.href = "https://github.com/UwU-37586109292"
+
+    const githubIcon = new Image();
+    githubIcon.src = Icon;
+    githubIcon.id = 'icon'
+    githubIcon.alt = "github-user-link"
+    githubIcon.style.display = 'block'
+
+
+    const githubIconLight = new Image();
+    githubIconLight.src = IconRevers;
+    githubIconLight.id = 'icon-light'
+    githubIconLight.alt = "github-user-link"
+    githubIconLight.style.display = 'none'
+
+    githubLink.appendChild(githubIcon)
+    githubLink.appendChild(githubIconLight)
+
+    githubIcon.addEventListener('mouseover', function () {
+        event.target.style.display = 'none'
+        document.getElementsByClassName('footer')[0].style.color = 'white'
+        document.getElementById('icon-light').style.display = 'block'
+    })
+    githubIconLight.addEventListener('mouseleave', function () {
+        event.target.style.display = 'none'
+        document.getElementsByClassName('footer')[0].style.color = 'black'
+        document.getElementById('icon').style.display = 'block'
+    })
+
+    footer.appendChild(githubLink)
+
+    const mainContent = document.getElementById('content')
     mainContent.appendChild(footer)
 }
