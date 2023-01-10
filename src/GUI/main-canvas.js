215384fe00631addfd803dc90b16fd4b89fd379e
UwU-37586109292 <kikucisko@gmail.com>
2023-01-10 16:07:44 +0100

    Each form is displayed only once

--- src/GUI/main-canvas.js
+++ src/GUI/main-canvas.js
@@ -2,74 +2,76 @@ import { projectList } from "../Model/project"
 import { addTodoToCurrentProject } from "../controller/app"
 import { getMainElement } from "./common"
 
-export default function showMainCanvas(){
+export default function showMainCanvas() {
     const mainContent = getMainElement()
 
     const canvas = document.createElement('main')
-    canvas.id =  'main'
+    canvas.id = 'main'
     canvas.classList.add('flex', 'column')
 
     // Add new todos
     appendAddTodoButton(canvas)
- 
+
     // pull existing todos
     appendExistingTodos(canvas)
 
     mainContent.appendChild(canvas)
 }
 
-function appendAddTodoButton(element){
+function appendAddTodoButton(element) {
     const button = document.createElement('button')
     button.innerText = "Add a task to be done"
     button.addEventListener("click", showAddTaskSection)
     element.appendChild(button)
 }
 
-function showAddTaskSection(){
+function showAddTaskSection() {
     const canvas = document.getElementById('main')
 
-    const form = document.createElement('form')
-    form.setAttribute('name', 'addTodo')
-    form.id = 'todoForm'
+    if (!document.getElementById('todoForm')) {
+        const form = document.createElement('form')
+        form.setAttribute('name', 'addTodo')
+        form.id = 'todoForm'
 
-    const inputTodoTitle = document.createElement('input')
-    inputTodoTitle.setAttribute('type', 'text')
-    inputTodoTitle.setAttribute('id','todoTitle')
-    inputTodoTitle.setAttribute('name', 'todoTitle')
+        const inputTodoTitle = document.createElement('input')
+        inputTodoTitle.setAttribute('type', 'text')
+        inputTodoTitle.setAttribute('id', 'todoTitle')
+        inputTodoTitle.setAttribute('name', 'todoTitle')
 
-    const saveButton = document.createElement('button')
-    saveButton.setAttribute('type', 'submit')
-    saveButton.innerText = 'Save task'
-    form.addEventListener('submit', function(event){
-        event.preventDefault()
-        addTodoToCurrentProject(document.getElementById('todoTitle').value)
-    })
+        const saveButton = document.createElement('button')
+        saveButton.setAttribute('type', 'submit')
+        saveButton.innerText = 'Save task'
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+            addTodoToCurrentProject(document.getElementById('todoTitle').value)
+        })
 
-    const discardButton = document.createElement('button')
-    discardButton.setAttribute('type', 'reset')
-    discardButton.innerText = 'Discard'
-    form.addEventListener('reset', function(){
-        hideAddTaskSection()
-    })
+        const discardButton = document.createElement('button')
+        discardButton.setAttribute('type', 'reset')
+        discardButton.innerText = 'Discard'
+        form.addEventListener('reset', function () {
+            hideAddTaskSection()
+        })
 
-    form.appendChild(inputTodoTitle)
-    form.appendChild(saveButton)
-    form.appendChild(discardButton)
-    canvas.appendChild(form)
+        form.appendChild(inputTodoTitle)
+        form.appendChild(saveButton)
+        form.appendChild(discardButton)
+        canvas.appendChild(form)
+    }
 }
 
-export function hideAddTaskSection(){
+export function hideAddTaskSection() {
     const form = document.getElementById('todoForm')
     form.remove()
 }
 
-export function refreshTodosList(){
+export function refreshTodosList() {
     const canvas = document.getElementById('main')
     document.querySelector('#main ul').remove()
     appendExistingTodos(canvas)
 }
 
-function appendExistingTodos(canvas){
+function appendExistingTodos(canvas) {
     const todoList = document.createElement('ul')
     projectList.getCurrentProject().getAllTasks().forEach(task => {
         const todo = document.createElement('li')
--- src/GUI/sidebar.js
+++ src/GUI/sidebar.js
@@ -3,70 +3,72 @@ import { addProjectFromForm } from "../controller/app"
 import { setProjectAsCurrent } from "../controller/app"
 import { getMainElement } from "./common"
 
-export default function showSidebar(){
+export default function showSidebar() {
     const sidebar = document.createElement('aside')
     sidebar.id = 'project-list'
     sidebar.classList.add('flex', 'column')
 
     appendAddProjectButton(sidebar)
-    
+
     appendAllProjects(sidebar)
 
     const mainContent = getMainElement()
     mainContent.appendChild(sidebar)
 }
 
-function appendAddProjectButton(element){
+function appendAddProjectButton(element) {
     const addProjectButton = document.createElement('button')
     addProjectButton.innerText = "Add project"
     addProjectButton.addEventListener('click', showAddProjectForm)
     element.appendChild(addProjectButton)
 }
 
-function showAddProjectForm(){
-    const form = document.createElement('form')
-    form.setAttribute('name', 'addProject')
-    form.id = 'projectForm'
+function showAddProjectForm() {
+    if (!document.getElementById('projectForm')) {
+        const form = document.createElement('form')
+        form.setAttribute('name', 'addProject')
+        form.id = 'projectForm'
 
-    const inputProjectName = document.createElement('input')
-    inputProjectName.setAttribute('type', 'text')
-    inputProjectName.setAttribute('id','projectTitle')
-    inputProjectName.setAttribute('name', 'projectTitle')
+        const inputProjectName = document.createElement('input')
+        inputProjectName.setAttribute('type', 'text')
+        inputProjectName.setAttribute('id', 'projectTitle')
+        inputProjectName.setAttribute('name', 'projectTitle')
 
-    const saveButton = document.createElement('button')
-    saveButton.setAttribute('type', 'submit')
-    saveButton.innerText = 'Save project'
-    form.addEventListener('submit', function(event){
-        event.preventDefault()
-        addProjectFromForm(document.getElementById('projectTitle').value)
-    })
+        const saveButton = document.createElement('button')
+        saveButton.setAttribute('type', 'submit')
+        saveButton.innerText = 'Save project'
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+            addProjectFromForm(document.getElementById('projectTitle').value)
+        })
 
-    const discardButton = document.createElement('button')
-    discardButton.setAttribute('type', 'reset')
-    discardButton.innerText = 'Discard'
-    form.addEventListener('reset', function(){
-        hideAddProjectForm()
-    })
+        const discardButton = document.createElement('button')
+        discardButton.setAttribute('type', 'reset')
+        discardButton.innerText = 'Discard'
+        form.addEventListener('reset', function () {
+            hideAddProjectForm()
+        })
 
-    form.appendChild(inputProjectName)
-    form.appendChild(saveButton)
-    form.appendChild(discardButton)
+        form.appendChild(inputProjectName)
+        form.appendChild(saveButton)
+        form.appendChild(discardButton)
 
-    document.getElementById('project-list').appendChild(form)
+        document.getElementById('project-list').appendChild(form)
+    }
 }
 
-export function hideAddProjectForm(){
+export function hideAddProjectForm() {
     document.getElementById('projectForm').remove()
 }
 
-function appendAllProjects(sidebarElement){
+function appendAllProjects(sidebarElement) {
     const listElement = document.createElement('ul')
-    
+
     projectList.getProjects().forEach(project => {
         const listed = document.createElement('li')
         listed.innerText = project.getTitle()
         listed.setAttribute('data-id', project.getId())
-        listed.addEventListener('click', function(){
+        listed.addEventListener('click', function () {
             setProjectAsCurrent(project)
         })
         listElement.appendChild(listed)
@@ -74,7 +76,7 @@ function appendAllProjects(sidebarElement){
     sidebarElement.appendChild(listElement)
 }
 
-export function refreshSidebar(){
+export function refreshSidebar() {
     document.querySelector('#project-list ul').remove()
     appendAllProjects(document.getElementById('project-list'))
 }
