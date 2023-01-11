4ef1b3e6887b1c134f1a6f78c06c21fd8bd5a2a5
UwU-37586109292 <kikucisko@gmail.com>
2023-01-11 13:29:07 +0100

    Add edit/delete icons to Project actons

--- src/GUI/common.js
+++ src/GUI/common.js
@@ -1,4 +1,22 @@
+import editIcon from '../assets/pencil.png';
+import deleteIcon from '../assets/delete.png';
 
 export function getMainElement(){
     return document.getElementById('content')
+}
+
+export function createEditIcon(){
+    return createIcon(editIcon)
+}
+
+export function createDeleteIcon(){
+    return createIcon(deleteIcon)
+}
+
+function createIcon(path){
+    const icon = new Image();
+    icon.src = path;
+    icon.style.display = 'block'
+    icon.classList.add('icon')
+    return icon
 }
--- src/GUI/sidebar.js
+++ src/GUI/sidebar.js
@@ -3,6 +3,7 @@ import { addProjectFromForm } from "../controller/app"
 import { setProjectAsCurrent } from "../controller/app"
 import { getMainElement } from "./common"
 import { deleteProject } from "../controller/app"
+import { createEditIcon, createDeleteIcon } from "./common"
 
 export default function showSidebar() {
     const sidebar = document.createElement('aside')
@@ -95,7 +96,7 @@ function createProjectTagElement(project) {
 function createEditProjectButton(project) {
     const button = document.createElement('button')
     button.classList.add('edit')
-    button.innerText = 'E'
+    button.appendChild(createEditIcon())
 
     //TODO: Add function that edits project
 
@@ -104,7 +105,7 @@ function createEditProjectButton(project) {
 
 function createDeleteProjectButton(project) {
     const button = document.createElement('button')
-    button.innerText = 'D'
+    button.appendChild(createDeleteIcon())
     button.addEventListener('click', function() {
         deleteProject(project)
     })
