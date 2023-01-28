116da3fb98426edbd4e78a7eba0d24b68e8c01e1
UwU-37586109292 <kikucisko@gmail.com>
2023-01-28 15:39:03 +0100

    Add ESLint

--- src/controller/app.js
+++ src/controller/app.js
@@ -1,104 +1,104 @@
-import { taskFactory } from "../Model/task";
-import { showAllProjectsOnCanvas } from "../GUI/main-canvas";
-import { hideAddTaskSection } from "../GUI/taskForm";
-import { refreshTodosList, removeTodoFromCanvas } from "../GUI/main-canvas";
-import { appendProjectToProjectList, refreshTaskCounter, removeProjectFromList, showProjectEmptyStateElement } from "../GUI/sidebar";
-import { projectFactory, projectList } from "../Model/project";
-import showHeader from '../GUI/header.js'
-import showSidebar from '../GUI/sidebar.js'
-import showFooter from '../GUI/footer.js'
-import showMainCanvas from '../GUI/main-canvas'
-import { showCurrentProjectsTasks, displayNewTaskOnList, showAddtodoButtonUnderProjectName } from '../GUI/main-canvas'
+import showFooter from '../GUI/footer';
+import showHeader from '../GUI/header';
+import showMainCanvas, {
+    displayNewTaskOnList, removeTodoFromCanvas, showAddtodoButtonUnderProjectName, showAllProjectsOnCanvas, showCurrentProjectsTasks
+} from '../GUI/main-canvas';
+import showSidebar, {
+    appendProjectToProjectList, refreshTaskCounter, removeProjectFromList, showProjectEmptyStateElement
+} from '../GUI/sidebar';
+import { hideAddTaskSection } from '../GUI/taskForm';
+import { projectFactory, projectList } from '../Model/project';
+import { taskFactory } from '../Model/task';
 
 export function initialize() {
-    // Default project and task setup for startup
-    const defaultProject = projectFactory('Inbox')
-    defaultProject.addTask(taskFactory("Do the laundry", 'whites', 'low', '01.01.2023', 'to do'))
-    const anotherProject = projectFactory('School')
-    projectList.addProjectToList(defaultProject)
-    projectList.setProjectAsCurrent(defaultProject)
-    projectList.addProjectToList(anotherProject)
+  // Default project and task setup for startup
+  const defaultProject = projectFactory('Inbox');
+  defaultProject.addTask(taskFactory('Do the laundry', 'whites', 'low', '01.01.2023', 'to do'));
+  const anotherProject = projectFactory('School');
+  projectList.addProjectToList(defaultProject);
+  projectList.setProjectAsCurrent(defaultProject);
+  projectList.addProjectToList(anotherProject);
 
-    // Set up main element
-    const content = document.createElement('div')
-    content.id = 'content'
-    document.body.appendChild(content)
+  // Set up main element
+  const content = document.createElement('div');
+  content.id = 'content';
+  document.body.appendChild(content);
 
-    // Show GUI
-    showHeader()
-    showSidebar()
-    showMainCanvas()
-    showFooter()
+  // Show GUI
+  showHeader();
+  showSidebar();
+  showMainCanvas();
+  showFooter();
 }
 
 export function addTodoToCurrentProject(newTask) {
-    addTodoToProject(newTask, projectList.getCurrentProject())
+  addTodoToProject(newTask, projectList.getCurrentProject());
 }
 
 export function addTodoToProject(todo, projectId) {
-    const project = projectList.getProjectById(projectId)
-    project.addTask(todo)
-    hideAddTaskSection()
-    showAddtodoButtonUnderProjectName(project)
-    displayNewTaskOnList(todo, project)
-    refreshTaskCounter()
+  const project = projectList.getProjectById(projectId);
+  project.addTask(todo);
+  hideAddTaskSection();
+  showAddtodoButtonUnderProjectName(project);
+  displayNewTaskOnList(todo, project);
+  refreshTaskCounter();
 }
 
 export function addProjectFromForm(projectTitle) {
-    const newProject = projectFactory(projectTitle);
-    projectList.addProjectToList(newProject)
-    setProjectAsCurrent(newProject)
-    appendProjectToProjectList(newProject)
+  const newProject = projectFactory(projectTitle);
+  projectList.addProjectToList(newProject);
+  setProjectAsCurrent(newProject);
+  appendProjectToProjectList(newProject);
 }
 
 export function setProjectAsCurrent(project) {
-    projectList.setProjectAsCurrent(project)
-    showCurrentProjectsTasks();
+  projectList.setProjectAsCurrent(project);
+  showCurrentProjectsTasks();
 }
 
 export function deleteProject(project) {
-    const currProjectId = projectList.getCurrentProject().getId()
-    const projectsDisplayed = document.querySelectorAll('.project-card').length
-    projectList.deleteProject(project)
-    removeProjectFromList(project.getId())
-    if (project.getId() === currProjectId || projectsDisplayed > 1) {
-        showAllProjectsOnCanvas()
-    }
-    if (projectList.getProjects().length === 0) {
-        showProjectEmptyStateElement()
-    }
+  const currProjectId = projectList.getCurrentProject().getId();
+  const projectsDisplayed = document.querySelectorAll('.project-card').length;
+  projectList.deleteProject(project);
+  removeProjectFromList(project.getId());
+  if (project.getId() === currProjectId || projectsDisplayed > 1) {
+    showAllProjectsOnCanvas();
+  }
+  if (projectList.getProjects().length === 0) {
+    showProjectEmptyStateElement();
+  }
 }
 
 export function deleteTodo(task) {
-    const allProjects = projectList.getProjects()
-    allProjects.forEach(project => {
-        const taskToDelete = project.getAllTasks().filter(taskToDelete => task.getId() === taskToDelete.getId())
-        if (taskToDelete.length > 0) {
-            project.removeTask(taskToDelete[0])
-        }
-    })
-    removeTodoFromCanvas(task.getId())
-    refreshTaskCounter()
+  const allProjects = projectList.getProjects();
+  allProjects.forEach((project) => {
+    const taskToDelete = project.getAllTasks().filter((taskToDelete) => task.getId() === taskToDelete.getId());
+    if (taskToDelete.length > 0) {
+      project.removeTask(taskToDelete[0]);
+    }
+  });
+  removeTodoFromCanvas(task.getId());
+  refreshTaskCounter();
 }
 
 export function toggleTaskStatus(task) {
-    const allProjects = projectList.getProjects()
-    allProjects.forEach(project => {
-        const taskToUpdate = project.getAllTasks().filter(taskToUpdate => task.getId() === taskToUpdate.getId())
-        if (taskToUpdate.length > 0) {
-            taskToUpdate[0].toggleStatus()
-        }
-    })
-    refreshTaskCounter()
+  const allProjects = projectList.getProjects();
+  allProjects.forEach((project) => {
+    const taskToUpdate = project.getAllTasks()
+      .filter((taskInProject) => task.getId() === taskInProject.getId());
+    if (taskToUpdate.length > 0) {
+      taskToUpdate[0].toggleStatus();
+    }
+  });
+  refreshTaskCounter();
 }
 export function editProjectFromForm(project, newTitle) {
-    project.setTitle(newTitle)
-    const currProjectId = projectList.getCurrentProject().getId()
-    const projectsDisplayed = document.querySelectorAll('.project-card').length
-    if (projectsDisplayed > 1) {
-        showAllProjectsOnCanvas()
-    }
-    else if (project.getId() === currProjectId) {
-        showCurrentProjectsTasks()
-    }
+  project.setTitle(newTitle);
+  const currProjectId = projectList.getCurrentProject().getId();
+  const projectsDisplayed = document.querySelectorAll('.project-card').length;
+  if (projectsDisplayed > 1) {
+    showAllProjectsOnCanvas();
+  } else if (project.getId() === currProjectId) {
+    showCurrentProjectsTasks();
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { taskFactory } from "./Model/task";
-import { projectFactory, projectList } from "./Model/project";
-import './style.css'
-import { initialize } from "./controller/app";
+import { initialize } from './controller/app';
+import './style.css';
 
-initialize()
+initialize();
 
-
-//<a href="https://www.flaticon.com/free-icons/pencil" title="pencil icons">Pencil icons created by Freepik - Flaticon</a>
-//<a href="https://www.flaticon.com/free-icons/trash" title="trash icons">Trash icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/pencil" title="pencil icons">Pencil icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/trash" title="trash icons">Trash icons created by Freepik - Flaticon</a>
 /* <a href="https://www.flaticon.com/free-icons/save" title="save icons">Save icons created by Yogi Aprelliyanto - Flaticon</a> */
 /* <a href="https://www.flaticon.com/free-icons/tick" title="tick icons">Tick icons created by Gregor Cresnar - Flaticon</a> */
 // <a href="https://www.flaticon.com/free-icons/close" title="close icons">Close icons created by ariefstudio - Flaticon</a>
